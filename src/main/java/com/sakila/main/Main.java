40c03db1ae07c2ce0f51efb64bc63d88a9ba58b1
JonathanAstrom <astrom.jonathan@gmail.com>
2022-03-31 14:31:21 +0200

    scenbyten

--- src/main/java/com/sakila/main/Main.java
+++ src/main/java/com/sakila/main/Main.java
@@ -2,6 +2,7 @@ package com.sakila.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,12 +10,13 @@ import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sakilamain.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sakilamain.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
